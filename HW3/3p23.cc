71ddb7c8b88f2b0df3d3b7d7933f6f9b3c718f34
ironsketch <michellejbergin@gmail.com>
2017-11-04 17:43:44 -0700

    poooooooop

--- HW3/3p23.cc
+++ HW3/3p23.cc
@@ -1,30 +1,78 @@
 /* 
  * Write a program to convert an infix expression that 
  * includes (, ), +, -, *, and / to postfix.
+ *
+ * Notes for my self: 
+ * 		if end paren - pop
+ * 		if higher presidence - pop
+ * 		else print numb
  */
 
 #include <iostream>
 #include <stack>
 #include <string>
+
 using namespace std;
-
-int main(){
-	stack<char> s;
-	string st = "A B *";
-
-	for(string::iterator it = st.begin(); it != st.end(); ++it){
-		if(*it == ' ')
-			continue;
-		if((int)*it > 47 && (int)*it < 58){
-			s.push(*it);
-		}else{
-			cout << s.top() << " ";
-			s.pop();
-			cout << s.top() << " ";
-			s.pop();
-			cout << *it << " ";
+bool isHigher(char c){
+	return c == '/' || c == '*' || c == '^';
+}
+void inToPost(string &s){
+	stack<char> stck;
+	for(int i = 0; i < s.size(); i++){
+		if (s[i] == ' '){
+				continue;
+		} else if(((int)s[i] > 47 && (int)s[i] < 58) && (int)s[i] != 94){
+			cout << s[i] << " ";
+		} else if(s[i] == ')'){
+			while(stck.top() != '('){
+				cout << stck.top() << " ";
+				stck.pop();
+			}
+			stck.pop();
+		} else if(s[i] == '+' || s[i] == '-'){
+			if(!stck.empty() && isHigher(stck.top())){
+				while(!stck.empty() && stck.top() != '('){
+					cout << stck.top() << " ";
+					stck.pop();
+				}
+				stck.push(s[i]);
+			} else {
+				stck.push(s[i]);
+			}
+		} else if(s[i] == '/' || s[i] == '*'){
+			if(!stck.empty() && stck.top() == '^'){
+				while(!stck.empty() && stck.top() != '('){
+					cout << stck.top() << " ";
+					stck.pop();
+				}
+				stck.push(s[i]);
+			} else {
+				stck.push(s[i]);
+			}
+		} else {
+			stck.push(s[i]);
 		}
 	}
+	while(!stck.empty()){
+		cout << stck.top() << " ";
+		stck.pop();
+	}
+	cout << endl;
+}
+
+int main(){
+	string st = "1 * 3";
+	string st2 = "1 * 2 ^ 3 + 4";
+	string st3 = "1 * (2 + 3 * 4) + 5";
+
+	cout << st << " is the infix thingy" << endl;
+	inToPost(st);
+
+	cout << "\n" << st2 << " is the infix thingy" << endl;
+	inToPost(st2);
+
+	cout << "\n" << st3 << " is the infix thingy" << endl;
+	inToPost(st3);
 
 	return 0;
 }
