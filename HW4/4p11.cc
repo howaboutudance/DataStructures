e0f40a0bac6e4964ee9eebaef3b7b1ac768df242
ironsketch <michellejbergin@gmail.com>
2017-11-14 09:15:32 -0800

    weeeee

--- HW4/4p11.cc
+++ HW4/4p11.cc
@@ -1,5 +1,32 @@
+#ifndef Btree_H
+#define Btree_H
 
 #include <iostream>
 #include <set>
 
+using namespace std;
 
+template <typename Object>
+
+class Btree{
+	public:
+		Btree(){
+			root = 
+		}
+
+		~Btree(){
+		}
+
+		bool add(){
+			node *temp = 
+		}
+	private:
+		node *root;
+
+		struct node{
+  			Object data;
+  			struct node *left;
+  			struct node *right;
+		};
+};
+#endif
