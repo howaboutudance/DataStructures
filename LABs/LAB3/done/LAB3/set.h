d63e2243e7b83d5760890b3afca0419284a30a71
Bergin <bermic30@ada.evergreen.edu>
2018-02-05 13:51:27 -0800

    oop

---
+++ LABs/LAB3/done/LAB3/set.h
@@ -0,0 +1,21 @@
+/* Spectacularly Energizing Tyrannosaurus's
+ * Data Structures
+ * LAB 3'
+ * Michelle Bergin
+ */
+
+#ifndef SET_H
+#define SET_H
+
+#include <iostream>
+#include "bst2.h"
+using namespace std;
+
+template <typename O>
+class set{
+	public:
+		
+	private:
+};
+
+#endif
